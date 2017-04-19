1572562b404a5a7ef24424fb8329c18666a4f3da
Ivan Kirichenko <librewind42@gmail.com>
2017-04-19 15:09:36 +0300

    Added additional facades for "Laravel Doctrine ORM"

--- config/app.php
+++ config/app.php
@@ -232,6 +232,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'EntityManager' => LaravelDoctrine\ORM\Facades\EntityManager::class,
+        'Registry' => LaravelDoctrine\ORM\Facades\Registry::class,
+        'Doctrine' => LaravelDoctrine\ORM\Facades\Doctrine::class,
 
     ],
 
